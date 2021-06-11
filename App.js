a67767d241c5af2bd0ed4b84bfda22a761ede745
Padmapriya29 <81081235+Padmapriya29@users.noreply.github.com>
2021-06-11 16:08:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import AppStack from "./Navigators/appStackNavigator";
+
+
+const App = () => {
+  return (
+    <NavigationContainer><AppStack/></NavigationContainer>
+  );
+};
+
+export default App;
